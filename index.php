1fed7fcc0bfbfb3427c2ca7d9d726113b3d610f1
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2025-01-17 13:39:23 -0300

    update

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php get_header(); ?>
 <a href="">testando</a>
 
+<h1>h1</h1>
+<h2>h2</h2>
+<h3>h3</h3>
+<h4>h4</h4>
+<h5>h5</h5>
+<h6>h6</h6>
+
+<p>Lorem ipsum dolor sit amet. </p>
+<span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe, ex! </span>
+<br>
+<img src="<?php echo get_template_directory_uri() ;?>/assets/img/nissan.jpg" alt="">
+
 <?php get_footer(); ?>
